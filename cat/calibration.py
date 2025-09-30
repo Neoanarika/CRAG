492a5953f30bb6f48acdd50a66e5e3b8bd13adff
Ang Ming Liang <angmingliang4017ic@gmail.com>
2025-10-01 02:20:34 +0800

    Implement AUC and correlation metric calculations
    Added functions for computing AUC and correlation metrics.

--- cat/calibration.py
+++ cat/calibration.py
@@ -1,118 +1,8 @@
-Last login: Tue Sep 30 15:42:38 on ttys028
-jamesmingliangang@Mac gstar inivdiusal porject % ssh a100azf
-Welcome to Ubuntu 22.04.4 LTS (GNU/Linux 5.15.0-141-generic x86_64)
-
- * Documentation:  https://help.ubuntu.com
- * Management:     https://landscape.canonical.com
- * Support:        https://ubuntu.com/pro
-
- System information as of Tue Sep 30 08:00:25 AM UTC 2025
-
-  System load:             0.4
-  Usage of /:              84.5% of 1.28TB
-  Memory usage:            38%
-  Swap usage:              68%
-  Processes:               591
-  Users logged in:         1
-  IPv4 address for enp1s0: 95.179.200.201
-  IPv6 address for enp1s0: 2001:19f0:7402:ad3:5400:4ff:fef4:a873
-
-  => There are 2 zombie processes.
-
- * Strictly confined Kubernetes makes edge and IoT secure. Learn how MicroK8s
-   just raised the bar for easy, resilient and secure K8s cluster deployment.
-
-   https://ubuntu.com/engage/secure-kubernetes-at-the-edge
-
-Expanded Security Maintenance for Applications is not enabled.
-
-146 updates can be applied immediately.
-6 of these updates are standard security updates.
-To see these additional updates run: apt list --upgradable
-
-27 additional security updates can be applied with ESM Apps.
-Learn more about enabling ESM Apps service at https://ubuntu.com/esm
-
-
-The list of available updates is more than a week old.
-To check for new updates run: sudo apt update
-
-2 updates could not be installed automatically. For more details,
-see /var/log/unattended-upgrades/unattended-upgrades.log
-
-*** System restart required ***
-Last login: Tue Sep 30 06:45:35 2025 from 203.117.22.140
-root@test-bha:~# cd mlexp/
-root@test-bha:~/mlexp# ls
-CRAG-RAG-Selection                           data                logreg-svrg-main      optimizer_comparison.png  RAG     svrg_istropic_guassian  trained
-crag_task_1_and_2_dev_sample30_v4.jsonl.bz2  GStar-Assignment-1  logreg-svrg-main.zip  output.png                reeval  tmp                     wandb
-root@test-bha:~/mlexp# cd CRAG-RAG-Selection/
-root@test-bha:~/mlexp/CRAG-RAG-Selection# source .venv/bin/activate
-(CRAG-RAG-Selection) root@test-bha:~/mlexp/CRAG-RAG-Selection# uv pip install "chonkie[semantic]"
-Resolved 26 packages in 281ms
-Prepared 6 packages in 75ms
-Installed 6 packages in 9ms
- + chonkie==1.3.1
- + markdown-it-py==4.0.0
- + mdurl==0.1.2
- + model2vec==0.6.0
- + pygments==2.19.2
- + rich==14.1.0
-(CRAG-RAG-Selection) root@test-bha:~/mlexp/CRAG-RAG-Selection# ls
-api_responses  CODE_OF_CONDUCT.md  data  example_data  local_evaluation.py  models   README.md         tokenizer
-calibration    CONTRIBUTING.md     docs  LICENSE       mock_api             prompts  requirements.txt  utils
-(CRAG-RAG-Selection) root@test-bha:~/mlexp/CRAG-RAG-Selection# tmux
-root@test-bha:~/mlexp/CRAG-RAG-Selection# vi local_evaluation.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection# vi local_evaluation.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection# cd models
-root@test-bha:~/mlexp/CRAG-RAG-Selection/models# ls
-dummy_model.py  __pycache__                      rag_llama_baseline.py  sentence-transformers  utils.py
-meta-llama      rag_knowledge_graph_baseline.py  README.md              user_config.py         vanilla_llama_baseline.py
-root@test-bha:~/mlexp/CRAG-RAG-Selection/models# vi rag_llama_baseline.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection/models# cd ..
-root@test-bha:~/mlexp/CRAG-RAG-Selection# ls
-api_responses  CODE_OF_CONDUCT.md  data  example_data  local_evaluation.py  models   README.md         tokenizer
-calibration    CONTRIBUTING.md     docs  LICENSE       mock_api             prompts  requirements.txt  utils
-root@test-bha:~/mlexp/CRAG-RAG-Selection# vi local_evaluation.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection# vi local_evaluation.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection# vi local_evaluation.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection# vi local_evaluation.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection# ls
-api_responses  CODE_OF_CONDUCT.md  data  example_data  local_evaluation.py  models   README.md         tokenizer
-calibration    CONTRIBUTING.md     docs  LICENSE       mock_api             prompts  requirements.txt  utils
-root@test-bha:~/mlexp/CRAG-RAG-Selection# cd models
-root@test-bha:~/mlexp/CRAG-RAG-Selection/models# ls
-dummy_model.py  __pycache__                      rag_llama_baseline.py  sentence-transformers  utils.py
-meta-llama      rag_knowledge_graph_baseline.py  README.md              user_config.py         vanilla_llama_baseline.py
-root@test-bha:~/mlexp/CRAG-RAG-Selection/models# vi rag_llama_baseline.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection/models# cd ..
-root@test-bha:~/mlexp/CRAG-RAG-Selection# vi local_evaluation.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection# vi local_evaluation.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection# vi local_evaluation.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection# python local_evaluation.py 
-  File "/root/mlexp/CRAG-RAG-Selection/local_evaluation.py", line 87
-    to match the score
-       ^^^^^
-SyntaxError: invalid syntax
-root@test-bha:~/mlexp/CRAG-RAG-Selection# vi local_evaluation.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection# python local_evaluation.py 
-Traceback (most recent call last):
-  File "/root/mlexp/CRAG-RAG-Selection/local_evaluation.py", line 14, in <module>
-    from loguru import logger
-ModuleNotFoundError: No module named 'loguru'
-root@test-bha:~/mlexp/CRAG-RAG-Selection# vi local_evaluation.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection# vi local_evaluation.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection# ls
-api_responses  CODE_OF_CONDUCT.md  data  example_data  local_evaluation.py  models   README.md         tokenizer
-calibration    CONTRIBUTING.md     docs  LICENSE       mock_api             prompts  requirements.txt  utils
-root@test-bha:~/mlexp/CRAG-RAG-Selection# cd calibration/
-root@test-bha:~/mlexp/CRAG-RAG-Selection/calibration# ls
-calibration.py
-root@test-bha:~/mlexp/CRAG-RAG-Selection/calibration# vi calibration.py 
-root@test-bha:~/mlexp/CRAG-RAG-Selection/calibration# ls
-calibration.py
-root@test-bha:~/mlexp/CRAG-RAG-Selection/calibration# vi calibration.py 
-
+import numpy as np
+import torch
+import pickle
+import os
+import gc
 from torch.distributions import Bernoulli
 from torch.optim import LBFGS
 from tqdm import tqdm
@@ -138,9 +28,9 @@ def trainer(parameters, optim, closure, n_iter=100, verbose=True):
         if iteration > 0:
             previous_parameters = [p.clone() for p in parameters]
             previous_loss = loss.clone()
-
+        
         loss = optim.step(closure)
-
+        
         if iteration > 0:
             d_loss = (previous_loss - loss).item()
             d_parameters = sum(
@@ -150,7 +40,7 @@ def trainer(parameters, optim, closure, n_iter=100, verbose=True):
             grad_norm = sum(torch.norm(p.grad, p=2).item() for p in parameters if p.grad is not None)
             if verbose:
                 pbar.set_postfix({"grad_norm": grad_norm, "d_parameter": d_parameters, "d_loss": d_loss})
-
+            
             if d_loss < 1e-5 and d_parameters < 1e-5 and grad_norm < 1e-5:
                 break
     return parameters
@@ -160,10 +50,122 @@ def compute_auc(probs, data, train_idtor, test_idtor):
     test_probs = probs[test_idtor.bool()]
     train_labels = data[train_idtor.bool()]
     test_labels = data[test_idtor.bool()]
-
+    
     train_auc = auroc(train_probs, train_labels)
     test_auc = auroc(test_probs, test_labels)
     print(f"train auc: {train_auc}")
     print(f"test auc: {test_auc}")
+    
+    return train_auc, test_auc
 
-"calibration.py" 171L, 6548B                                                                                                                                       53,4           4%
+def compute_cttcorr(probs, data, train_idtor, test_idtor):
+    train_probs  = probs.clone()
+    test_probs   = probs.clone()
+    train_labels = data.clone()
+    test_labels  = data.clone()
+
+    train_mask = ~train_idtor.bool()
+    train_probs[train_mask]  = float('nan')
+    train_labels[train_mask] = float('nan')
+
+    test_mask = ~test_idtor.bool()
+    test_probs[test_mask]   = float('nan')
+    test_labels[test_mask]  = float('nan')
+    
+    train_prob_ctt = torch.nanmean(train_probs, dim=1).detach().cpu().numpy()
+    train_label_ctt = torch.nanmean(train_labels, dim=1).detach().cpu().numpy()
+    train_mask = ~np.isnan(train_prob_ctt) & ~np.isnan(train_label_ctt)
+    train_cttcorr = pearsonr(train_prob_ctt[train_mask], train_label_ctt[train_mask]).statistic
+    
+    test_prob_ctt = torch.nanmean(test_probs, dim=1).detach().cpu().numpy()
+    test_label_ctt = torch.nanmean(test_labels, dim=1).detach().cpu().numpy()
+    test_mask = ~np.isnan(test_prob_ctt) & ~np.isnan(test_label_ctt)
+    test_cttcorr = pearsonr(test_prob_ctt[test_mask], test_label_ctt[test_mask]).statistic
+    
+    print(f"train cttcorr: {train_cttcorr}")
+    print(f"test cttcorr: {test_cttcorr}")
+
+    return train_cttcorr, test_cttcorr
+
+if __name__ == "__main__":
+    with open(f"../data/resmat.pkl", "rb") as f:
+        results = pickle.load(f)
+        
+    # data_withnan, missing=nan
+    # data_withneg1, missing=-1
+    # data_with0, missing=0
+    data_withnan = torch.tensor(results.values, dtype=torch.float, device=device)
+    data_withneg1 = data_withnan.nan_to_num(nan=-1.0)
+    data_idtor = (data_withneg1 != -1).to(float)
+    data_with0 = data_withneg1 * data_idtor # -1 -> 0
+    n_test_takers, n_items = data_with0.shape
+    scenarios = results.columns.get_level_values("scenario").unique()
+
+    # save dict
+    metric_results = defaultdict(dict)
+
+    vis_resmat_dir = "../result/visualize_resmat"
+    os.makedirs(vis_resmat_dir, exist_ok=True)
+
+    # overall stats
+    print("Number of test takers:", results.shape[0])
+    print("Number of items:", results.shape[1])
+    print("Number of scenarios:", results.columns.get_level_values("scenario").nunique())
+
+    
+    # data_idtor = train_idtor + test_idtor
+    # apply random train/test mask to the matrix, and ensure no one row or column is fully masked
+    valid_condition = False
+    trial = 0
+    while not valid_condition:
+        train_idtor = torch.bernoulli(data_idtor * 0.8).int()
+        test_idtor = data_idtor - train_idtor
+        valid_condition = (train_idtor.sum(axis=1) != 0).all() and (train_idtor.sum(axis=0) != 0).all()
+        print(f"trial {trial} valid condition: {valid_condition}")
+        trial += 1
+
+    # fit z
+    B = 50000
+    optimized_zs = []
+    thetas_nuisance = torch.randn(150, n_test_takers, device=device)
+    for i in tqdm(range(0, n_items, B)):
+        data_batch = data_with0[:, i:i+B]
+        train_idtor_batch = train_idtor[:, i:i+B]
+        current_B = data_batch.shape[1]
+        z_i = torch.randn(current_B, requires_grad=True, device=device)
+        optim_z_i = LBFGS([z_i], lr=0.1, max_iter=20, history_size=10, line_search_fn="strong_wolfe")
+        def closure_z_i():
+            optim_z_i.zero_grad()
+            probs = torch.sigmoid(thetas_nuisance[:, :, None] + z_i[None, None, :])
+            loss = -(Bernoulli(probs=probs).log_prob(data_batch)*train_idtor_batch).mean()
+            loss.backward()
+            return loss
+        z_i_optimized = trainer([z_i], optim_z_i, closure_z_i)[0].detach()
+        optimized_zs.append(z_i_optimized)
+    zs = torch.cat(optimized_zs)
+
+    # fit theta
+    thetas = torch.randn(n_test_takers, requires_grad=True, device=device)
+    optim_theta = LBFGS([thetas], lr=0.1, max_iter=20, history_size=10, line_search_fn="strong_wolfe")
+    def closure_theta():
+        optim_theta.zero_grad()
+        probs = torch.sigmoid(thetas[:, None] + zs[None, :])
+        loss = -(Bernoulli(probs=probs).log_prob(data_with0)*train_idtor).mean()
+        loss.backward()
+        return loss
+    thetas = trainer([thetas], optim_theta, closure_theta)[0]
+
+    # calculate metrics
+    probs = torch.sigmoid(thetas[:, None] + zs[None, :])
+
+    train_auc, test_auc = compute_auc(probs, data_with0, train_idtor, test_idtor)
+    metric_results["combined_data"]["train_auc"] = train_auc.item()
+    metric_results["combined_data"]["test_auc"] = test_auc.item()
+
+    train_cttcorr, test_cttcorr = compute_cttcorr(probs, data_with0, train_idtor, test_idtor)
+    metric_results["combined_data"]["train_cttcorr"] = train_cttcorr.item()
+    metric_results["combined_data"]["test_cttcorr"] = test_cttcorr.item()
+
+    del optim_theta, thetas, z_i, thetas_nuisance, optim_z_i
+    gc.collect()
+    torch.cuda.empty_cache()
